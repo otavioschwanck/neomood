1b6e9edaf867ea5eb5166b35d3523b0a87748311
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2025-12-07 01:23:00 +0000

    upgrade plugins

--- lua/plugins/github-pr-reviewer.lua
+++ lua/plugins/github-pr-reviewer.lua
@@ -5,11 +5,11 @@ return {
     event = "VeryLazy",
     opts = {
       picker = "fzf-lua",
-      open_files_on_review = true,
+      -- open_files_on_review = true,
     },
     keys = {
-      { "<leader>p", "<cmd>PRReviewMenu<cr>",    desc = "PR Review Menu" },
-      { "<leader>p", "<cmd>PRSuggestChange<CR>", desc = "Suggest change", mode = "v" }
+      { "<leader>p", "<cmd>PRReviewMenu<cr>",          desc = "PR Review Menu" },
+      { "<leader>p", ":<C-u>'<,'>PRSuggestChange<CR>", desc = "Suggest change", mode = "v" }
     }
   },
 }
